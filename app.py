12f40ef9e5a18901bee5904678ece7499f04b042
Adriana Cordero Valencia <adrianacorderoval04@gmail.com>
2026-01-26 22:56:40 -0400

    login completo

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta"
 
+#login manager
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+def get_db_connection():
+    conn = sqlite3.connect('form_hv.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 def init_database():
-    conn = sqlite3.connect("form_hv.db")
-    cursor = conn.cursor()
-    cursor.execute(
+    conn = get_db_connection()
+    conn.execute(
         """
         CREATE TABLE IF NOT EXISTS datos(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,7 +42,7 @@ def init_database():
 """
     )
 
-    cursor.execute(
+    conn.execute(
         """
         CREATE TABLE IF NOT EXISTS experiencia(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -47,14 +58,73 @@ def init_database():
     """
     )
 
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+        );
+        """
+    )
+
     conn.commit()
     conn.close()
 
 init_database()
 
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    @staticmethod
+    def get_by_id(user_id):
+        conn = get_db_connection
+        user = conn.execute('SELECT * FROM users WHERE id=?',(user_id, )).fetchone()
+        conn.close()
+        if user:
+            return User(user['id'], user['username'],user['password'])
+        return None
+    
+    @staticmethod
+    def get_by_username(username):
+        conn = get_db_connection()
+        user = conn.execute('SELECT * FROM users WHERE username=?', (username, )).fetchone()
+        conn.close()
+        if user:
+            return User(user['id'], user['username'], user['password'])
+        return None
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.get_by_username(username)
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Inicio de sesión exitoso','success')
+            return redirect(url_for())
+        else:
+            flash('Credenciales inválidas','danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Has cerrado sesión','info')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
